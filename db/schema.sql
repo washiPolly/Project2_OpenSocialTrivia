5c8934fc304b93d540d94fbb21d02b6fdfd21829
washiPolly <pollylsu@gmail.com>
2020-06-04 20:26:19 -0700

    updated config file with jaws production

--- db/schema.sql
+++ db/schema.sql
@@ -3,11 +3,11 @@ This file is intended to help developers get their SQL Databases setup correctly
 It is important since other developers will be unlikely to have the same database or tables setup already.
 */
 
-/* Create and use the starwars db */
-/* DROP DATABASE IF EXISTS `trivia_db`;
-CREATE DATABASE `trivia_db`;
 
-*/
+-- DROP DATABASE IF EXISTS `trivia_db`;
+-- CREATE DATABASE `trivia_db`;
+
+
 USE `trivia_db`;
 
 
